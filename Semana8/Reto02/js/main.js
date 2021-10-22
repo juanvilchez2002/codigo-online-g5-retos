d644044bae94ed0dcfa657cac930cd76ed0ffa4d
Juan A. Vilchez Huarcaya <juanvilchez2002@gmail.com>
2021-10-21 23:24:23 -0500

    Reto 02 - Cumplido

--- Semana8/Reto02/js/main.js
+++ Semana8/Reto02/js/main.js
@@ -1,44 +1,65 @@
-
-
-
+//definiendo dnd obtendremos la data
 const url1 = "https://616b5ec116c3fa0017171696.mockapi.io/usuario_prueba";
 
-const getData = async()=>{
-    try{
-       let respt = await fetch(url1);
-       let datos = await respt.json();
-       console.table(datos);
-       return datos;
-    }catch(err){
-        console.log(err);
+//definiendo la clase
+class toDoAPP{
+    //definiendo el contructor
+    constructor(direc){
+        this.direc = direc;
+        //llamando una función para al inicializar la clase
+        this.mostrarData()
     }
-}
 
+    //definiendo la función que obtendra la data
+    async obtenerData(){
+        try {
+            let respuesta = await fetch(this.direc);
+            let datos = await respuesta.json();
+            console.table(datos);
+            return datos;
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-//creando un selector para la etiqueda <body></body>
-let body = document.querySelector('body');
+    //defiinendo la función que mostrara la data
+    async mostrarData(){
+        try {
 
-getData()
-    .then((misDatos)=>{
-        let htmlProd = "";
-        misDatos.forEach(date => {
-            htmlProd += `
-                <div class="card col-lg-4 col-md-6 col-sm-12">
-                    <img src="${date.foto}" class="card-img-top mt-2" alt="avatar de ${date.nombre}" >
-                    <div class="card-body text-center">
-                        <h5 class="card-title">${date.nombre}</h5>
-                        <p class="card-text">${date.correo}</p>
-                    </div>
-                </div>`
-        });
-        console.log(htmlProd);
-        body.innerHTML = `
+            let htmlProd ="";
+
+            let misDatos = await this.obtenerData();
+
+            
+            misDatos.forEach(date => {
+                htmlProd += `
+                    <div class="card col-lg-4 col-md-6 col-sm-12">
+                        <img src="${date.foto}" class="card-img-top mt-2" alt="avatar de ${date.nombre}" >
+                        <div class="card-body text-center">
+                            <h5 class="card-title">${date.nombre}</h5>
+                            <p class="card-text">${date.correo}</p>
+                        </div>
+                    </div>`
+            });
+
+            body.innerHTML = `
                             <main class="container mt-2">
                                 <div class="row">                                    
                                     ${htmlProd}
                                 <div>
                             </main>`;
-        console.log(body.innerHTML);
-    })
-    .catch(err => console.log(err));
 
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+//creando un selector para la etiqueda <body></body>
+let body = document.querySelector('body');
+
+//creando un objeto de la clase
+let nuevaData = new toDoAPP(url1)
+
+console.log(nuevaData);
+//nuevaData.mostrarData();
